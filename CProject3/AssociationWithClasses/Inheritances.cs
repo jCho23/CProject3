c569ef269469b9d6c4fe67e2a5da152c14f909aa
June Cho <junecho.ucb@gmail.com>
2017-09-25 18:56:31 -0700

    Inheritance notes

--- CProject3/AssociationWithClasses/Inheritances.cs
+++ CProject3/AssociationWithClasses/Inheritances.cs
@@ -1,4 +1,20 @@
-﻿using System;
+﻿////Inheritance = A kind of relationship between two classes that allows one to inherit code from the other
+////*Benefits
+//// 1) Code Re-USe = Encapsulates one classs and use that later for another classs
+//// 2) Polymorphic behavior = 
+
+////Syntax Inheritance Demo
+//public class PresentationObject
+//{
+////Common Shared Code    
+//}
+
+//public class Text:PresentationObject
+//{
+////Code Specific to Text 
+//}
+
+using System;
 namespace CProject3.AssociationWithClasses
 {
     public class Inheritances
