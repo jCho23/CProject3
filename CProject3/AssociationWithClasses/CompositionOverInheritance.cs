cd1b6a772f651bbfa3a98482a63addd120316cc2
June Cho <junecho.ucb@gmail.com>
2017-09-25 18:59:09 -0700

    Inheritance vs Composition Notes

--- CProject3/AssociationWithClasses/CompositionOverInheritance.cs
+++ CProject3/AssociationWithClasses/CompositionOverInheritance.cs
@@ -1,10 +1,17 @@
-﻿using System;
-namespace CProject3.AssociationWithClasses
-{
-    public class CompositionOverInheritance
-    {
-        public CompositionOverInheritance()
-        {
-        }
-    }
-}
+﻿////Problems with Inheritance
+//// 1) Easily abused by amateur designers / developers
+//// 2) Large Hierarchies
+//// 3) Fragility
+//// 4) Tightly Coupling 
+
+////Summary
+//// Inheritance
+////*Pros: Code re-use, easier to understand
+////*Cons: Tightly coupled, fragile, can be abused
+
+////Composition= Any Inheritance relationship can be translated to Composition
+////*Benefits / Pros
+//// 1) Great Flexiblity
+//// 2) Eventually Loose Coupling
+////*Cons
+//// 1) Hard to understand  
