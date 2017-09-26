1938986eee18e27d3f30d04c9c13d6f295e6869d
June Cho <junecho.ucb@gmail.com>
2017-09-25 18:46:45 -0700

    Added Composition Notes

--- CProject3/AssociationWithClasses/Compositions.cs
+++ CProject3/AssociationWithClasses/Compositions.cs
@@ -1,4 +1,23 @@
-﻿using System;
+﻿////Composition= Relationship between Two Classes that allows one to contain the other
+////*Benefits
+////1) Code Re-USe
+////2) Flexibilty
+////3) A means to Loose-Coupling Apps
+
+////Composition Syntax
+//public class Installer
+//{
+////Notice that this is Private Field for Composition
+//private Logger _logger;
+
+////Here, the Contructor of the Installer Class returns a logger Object, which we use to Intialize the Private field above
+//public Installer(Logger logger)
+//{
+//    _logger = logger;
+//}
+//}
+
+using System;
 namespace CProject3.AssociationWithClasses
 {
     public class Compositions
