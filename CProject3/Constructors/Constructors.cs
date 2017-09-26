3fd7c74f57d576892bda126712e58697161a84cf
June Cho <junecho.ucb@gmail.com>
2017-09-26 08:56:07 -0700

    fin Demo1

--- CProject3/Constructors/Constructors.cs
+++ CProject3/Constructors/Constructors.cs
@@ -1,10 +1,19 @@
-﻿using System;
-namespace CProject3.Constructors
-{
-    public class Constructors
-    {
-        public Constructors()
-        {
-        }
-    }
-}
+﻿////Constructor = A Method that is called when an instance of a class is created
+////Why do we need a Constructor? = To put an object in an early state
+
+
+////Demo 1
+////This is how to Declare a Constructor
+////The Constructor is the same name as the class (*REQUIREMENT*)
+	//public class Constructors
+	//{
+	      ////This is the Constructor
+	      ////Constuctors, unlike other Methods, does not have a return type
+	      ////If you need to initialize an Object upon creation, use a constuctor 
+	//    public Constructors()
+	//    {
+	//    }
+	//}
+
+
+
