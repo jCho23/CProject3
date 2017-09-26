7b1c20e74a245fff1fc01a33f663ff3fd915dccf
June Cho <junecho.ucb@gmail.com>
2017-09-26 08:59:01 -0700

    fin Demo 2

--- CProject3/Constructors/Constructors.cs
+++ CProject3/Constructors/Constructors.cs
@@ -5,15 +5,28 @@
 ////Demo 1
 ////This is how to Declare a Constructor
 ////The Constructor is the same name as the class (*REQUIREMENT*)
-	//public class Constructors
-	//{
-	      ////This is the Constructor
-	      ////Constuctors, unlike other Methods, does not have a return type
-	      ////If you need to initialize an Object upon creation, use a constuctor 
-	//    public Constructors()
-	//    {
-	//    }
-	//}
+//public class Constructors
+//{
+////This is the Constructor
+////Constuctors, unlike other Methods, does not have a return type
+////If you need to initialize an Object upon creation, use a constuctor 
+//    public Constructors()
+//    {
+//    }
+//}
 
+////Demo 2
+//public class Customer
+//{
+////This Name is set below
+//    public string Name;
 
+////Here, "string name" is a Parameter
+////So we get the name of the customer and set the string up above
+//    public Customer(string name)
+//    {
+//        //"this" = References the current Object and ties it to the "Name" up above
+//        this.Name = name;
+//    }
+//}
 
