27835d0e356266b312155f9f4483306ae3c653de
June Cho <junecho.ucb@gmail.com>
2017-09-25 18:45:06 -0700

    fin ClassCoupling

--- CProject3/AssociationWithClasses/ClassCoupling.cs
+++ CProject3/AssociationWithClasses/ClassCoupling.cs
@@ -1,10 +1,13 @@
-﻿using System;
-namespace CProject3.AssociationWithClasses
-{
-    public class ClassCoupling
-    {
-        public ClassCoupling()
-        {
-        }
-    }
-}
+﻿////Class Coupling = A measure of how interconnected classes and subsystems are
+
+////*BAD* Tightly Coupled= An app that have Classes tightly related to each other 
+////*GOOD* Loosely Coupled= Not Dependent on other Classes
+
+////How to Design Loosely Coupled?
+////*Encapulation= Using Access Modifiers, restrict what other Classes see
+////*Relationship between Classes
+////*Interfaces 
+
+////Types of Relationships
+////*Inheritance
+////*Compositio
