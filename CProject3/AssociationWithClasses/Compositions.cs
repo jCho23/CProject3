1c267a095a411a34d1dfbd8e88355d257bd292d9
June Cho <junecho.ucb@gmail.com>
2017-09-25 18:48:37 -0700

    Fin Compositions Demo

--- CProject3/AssociationWithClasses/Compositions.cs
+++ CProject3/AssociationWithClasses/Compositions.cs
@@ -19,11 +19,65 @@
 
 using System;
 namespace CProject3.AssociationWithClasses
+
 {
-    public class Compositions
-    {
-        public Compositions()
-        {
-        }
-    }
+    ////Let's start by creating a Logger Class where we have the common functionality
+    public class Logger
+	{
+		public void Log(string message)
+		{
+			Console.WriteLine(message);
+		}
+	}
+
+	////This Method is responsible to Migrate the Data
+	public class DBMigrator
+	{
+		////Here, we created a Constructor and used Composition
+		private readonly Logger _logger;
+
+		public DBMigrator(Logger logger)
+		{
+			_logger = logger;
+		}
+
+		public void Migrate()
+		{
+			_logger.Log("We are Migrating...");
+		}
+	}
+
+	////Installer
+	public class Installer
+	{
+		private readonly Logger _logger;
+
+		public Installer(Logger logger)
+		{
+			_logger = logger;
+		}
+
+		public void Install()
+		{
+			_logger.Log("We are Installing the Application.");
+		}
+	}
+
+	public class Compositions
+	{
+		//static void Main(string[] args)
+		//{
+		////Here we need to pass the Logger Object
+		////*This is one way to do it 
+		//var dbMigrator = new DBMigrator(new Logger());
+
+		////This is the second way to pass the Logger Object
+		//var logger = new Logger();
+		//var installer = new Installer(logger);
+
+		//dbMigrator.Migrate();
+
+		//installer.Install();
+		//}
+	}
 }
