95f2764977b895e9044d93ec5b6e3489333372e4
June Cho <junecho.ucb@gmail.com>
2017-09-25 18:58:42 -0700

    Added CompositionOverInheritance

---
+++ CProject3/AssociationWithClasses/CompositionOverInheritance.cs
@@ -0,0 +1,10 @@
+﻿using System;
+namespace CProject3.AssociationWithClasses
+{
+    public class CompositionOverInheritance
+    {
+        public CompositionOverInheritance()
+        {
+        }
+    }
+}
