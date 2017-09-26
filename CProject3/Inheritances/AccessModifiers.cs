d9328e352505c699cad34f7699dafcc20d018776
June Cho <junecho.ucb@gmail.com>
2017-09-25 20:20:50 -0700

    Added Access Modifiers notes

--- CProject3/Inheritances/AccessModifiers.cs
+++ CProject3/Inheritances/AccessModifiers.cs
@@ -1,4 +1,11 @@
-﻿
+﻿////BlackBox = Limited visiblity from the outside-->Make Classes like this
+
+////Access Modifiers
+////* public = Accessible from everywhere
+////* private = Accessible only from the Class
+////* protected = Accessible only from the Class and its derived Classes (breaks encapsulation)
+////* internal = Accessible only from the same assembly
+////* protected internal = Accessible only from the same assebly or any derived Classes     
 
 using System;
 namespace CProject3.Inheritances
