dd76c40f4ef2d69ec237126a6380db22f359c464
June Cho <junecho.ucb@gmail.com>
2017-09-26 18:44:27 -0700

    Added Customer Amazon

---
+++ Amazon/Customer.cs
@@ -0,0 +1,15 @@
+﻿using System;
+
+namespace Amazon
+{
+    public class Customer
+    {
+        public int Id { get; set; }
+        public string Name { get; set; }
+
+        public void Promote()
+        {
+           Console.WriteLine("Promote Logic Changed.");
+        }
+    }
+}
