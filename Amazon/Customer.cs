2df7e8e4b4b4c43b2a843e5c5911dbc0ef7087ba
June Cho <junecho.ucb@gmail.com>
2017-09-26 18:49:04 -0700

    Moved to Amazon.Customer

--- Amazon/Customer.cs
+++ Amazon/Customer.cs
@@ -2,6 +2,15 @@
 
 namespace Amazon
 {
+    public class RateCalculator
+    {
+        public int Calculate(Customer customer)
+        {
+            return 0;
+        }
+   
+    }
+
     public class Customer
     {
         public int Id { get; set; }
@@ -9,6 +18,10 @@ namespace Amazon
 
         public void Promote()
         {
+            ////Note, this is not best practice to instanticate a new RateCalculator here
+            var calculator = new RateCalculator();
+            var rating = calculator.Calculate(this);
+
            Console.WriteLine("Promote Logic Changed.");
         }
     }
--- CProject3/Inheritances/AccessModifiers.cs
+++ CProject3/Inheritances/AccessModifiers.cs
@@ -10,34 +10,35 @@
 using System;
 namespace CProject3.Inheritances
 {
-    public class Customer
-    {
-        ////Here, we are defining Properties of the Customer Class
-        public int Id { get; set; }
-        public string Name { get; set; }
+    ////Moved to Amazon.Customer
+    //public class Customer
+    //{
+    //    ////Here, we are defining Properties of the Customer Class
+    //    public int Id { get; set; }
+    //    public string Name { get; set; }
 
-        public void Promote()
-        {
-            ////We are calling the CalculateRating Method below
-            var rating = CalculateRating();
+    //    public void Promote()
+    //    {
+    //        ////We are calling the CalculateRating Method below
+    //        var rating = CalculateRating();
 
-            ////From the Return of CalculateRating Method we can make Decisions
-            if (rating==0)
-                Console.WriteLine("Promoted to Level 1");
-            else
-                Console.WriteLine("Promoted to Level 2");
-        }
+    //        ////From the Return of CalculateRating Method we can make Decisions
+    //        if (rating==0)
+    //            Console.WriteLine("Promoted to Level 1");
+    //        else
+    //            Console.WriteLine("Promoted to Level 2");
+    //    }
 
-        ////This CalculateRating is the Implementation Detail of the CustomerClass
-        ////Notice that we changed this Method to Private to encapsulate it from other Classes
-        ////Now, we only need to changed the code once when it is getting called in the above Class
-        ////More importantly, we don't want other Classes to have dependencies from this Method   
-        //public int CalculateRating()
-        private int CalculateRating()
-        {
-            return 0;
-        }
-    }
+    //    ////This CalculateRating is the Implementation Detail of the CustomerClass
+    //    ////Notice that we changed this Method to Private to encapsulate it from other Classes
+    //    ////Now, we only need to changed the code once when it is getting called in the above Class
+    //    ////More importantly, we don't want other Classes to have dependencies from this Method   
+    //    //public int CalculateRating()
+    //    private int CalculateRating()
+    //    {
+    //        return 0;
+    //    }
+    //}
 
     ////This GoldCustomer Class derives from the Customer Class
     public class GoldCustomer : Customer
