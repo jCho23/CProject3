534f12c8b5e6d254c4d52607134da7f25af99b90
June Cho <junecho.ucb@gmail.com>
2017-09-27 10:59:24 -0700

    Properties notes

---
+++ CProject3/PropertiesInClasses/PropertiesInClasses.cs
@@ -0,0 +1,13 @@
+﻿////Properties= A class member that encapsulates a getter/setter for accessing a field
+//// Why do we need Properties? = To create a getter/setter with less code
+
+using System;
+namespace CProject3.PropertiesInClasses
+{
+    public class PropertiesInClasses
+    {
+        public PropertiesInClasses()
+        {
+        }
+    }
+}
