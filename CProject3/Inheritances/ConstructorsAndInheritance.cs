5908e8d098b9b34ae6f90640b5f24311b62dcad0
June Cho <junecho.ucb@gmail.com>
2017-09-28 12:54:11 -0700

    Parameterless Constructor

--- CProject3/Inheritances/ConstructorsAndInheritance.cs
+++ CProject3/Inheritances/ConstructorsAndInheritance.cs
@@ -2,14 +2,25 @@
 ////* Base Class constructors are always executed first
 ////* Base Class constructors are not inherited 
 
-
 using System;
+
 namespace CProject3.Inheritances
 {
+    public class Vehicle
+    {
+        ////Parameterless Constructor
+        public Vehicle()
+        {
+            Console.WriteLine("Vehicle is being initialized");
+        }
+    }
+
+
     public class ConstructorsAndInheritance
     {
-        public ConstructorsAndInheritance()
+        static void Main(string[] args)
         {
+            
         }
     }
 }
