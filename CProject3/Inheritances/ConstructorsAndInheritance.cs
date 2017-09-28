29bbf79aec99da36b09567d09b4307bbc4863653
June Cho <junecho.ucb@gmail.com>
2017-09-28 09:32:29 -0700

    Constructor Inheritance notes

--- CProject3/Inheritances/ConstructorsAndInheritance.cs
+++ CProject3/Inheritances/ConstructorsAndInheritance.cs
@@ -1,4 +1,9 @@
-﻿using System;
+﻿////Constructor Inheritance
+////* Base Class constructors are always executed first
+////* Base Class constructors are not inherited 
+
+
+using System;
 namespace CProject3.Inheritances
 {
     public class ConstructorsAndInheritance
