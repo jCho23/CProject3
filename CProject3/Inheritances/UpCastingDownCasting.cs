5af8fcb106eb6a6d753fb9078bb95a510231d454
June Cho <junecho.ucb@gmail.com>
2017-09-30 20:19:39 -0700

    Upcasting

--- CProject3/Inheritances/UpCastingDownCasting.cs
+++ CProject3/Inheritances/UpCastingDownCasting.cs
@@ -1,4 +1,12 @@
-﻿using System;
+﻿////Upcasting
+Circle circle = new Circle();
+Shape shape = circle;
+
+
+
+
+
+using System;
 namespace CProject3.Inheritances
 {
     public class UpCastingDownCasting
