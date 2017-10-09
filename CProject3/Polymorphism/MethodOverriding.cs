4abbcf3802b04cab182a20e68bf0dc75314fb245
June Cho <junecho.ucb@gmail.com>
2017-10-08 18:45:16 -0700

    CleanUp

--- CProject3/Polymorphism/MethodOverriding.cs
+++ CProject3/Polymorphism/MethodOverriding.cs
@@ -1,25 +1,27 @@
 ﻿////Method Overriding = Modifiying the implementation of an inherited method
 
-public class Shape
-{
-    public virtual void Draw()
-    {
+//public class Shape
+//{
+    ////Virtual= Ability to change this Methods' implementation in the dervied class
+    //public virtual void Draw()
+    //{
         ////Default implementation
-    }
-}
+//    }
+//}
 
-public class Circle : Shape
-{
-    public override void Draw()
-    {
+//public class Circle : Shape
+//{
+    ////Override= Changes the implementation of the Method above
+    //public override void Draw()
+    //{
         ////New implemenation
-    }
-}
+//    }
+//}
 
-public class Image : Shape
-{
+//public class Image : Shape
+//{
 
-}
+//}
 
 using System;
 namespace CProject3.Polymorphism
