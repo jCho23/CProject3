7a407719544aa1856e422e176bb8cce852eb846e
June Cho <junecho.ucb@gmail.com>
2017-10-08 20:09:00 -0700

    created MethodOverriding class

--- CProject3/Polymorphism/MethodOverriding.cs
+++ CProject3/Polymorphism/MethodOverriding.cs
@@ -108,7 +108,19 @@ namespace CProject3.Polymorphism
         }
     }
 
+    class MethodOverriding
+    {
+        static void Main(string[] args)
+        {
+            var shapes = new List<Shape>();
+            shapes.Add(new Circle());
+            shapes.Add(new Rectangle());
 
+            var canvas = new Canvas();
+            canvas.DrawShapes(shapes);
+        }
+
+    }
 
    
 }
