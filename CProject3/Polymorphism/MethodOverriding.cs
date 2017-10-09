47e16784c62f64b411c04426b482c5cd950496b4
June Cho <junecho.ucb@gmail.com>
2017-10-08 18:28:25 -0700

    Method Overriding defined

--- CProject3/Polymorphism/MethodOverriding.cs
+++ CProject3/Polymorphism/MethodOverriding.cs
@@ -1,4 +1,6 @@
-﻿using System;
+﻿////Method Overriding = Modifiying the implementation of an inherited method
+
+using System;
 namespace CProject3.Polymorphism
 {
     public class MethodOverriding
