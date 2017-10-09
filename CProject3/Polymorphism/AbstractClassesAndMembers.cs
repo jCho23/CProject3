3e255f617903e945627e96c37c5eda40286882b2
June Cho <junecho.ucb@gmail.com>
2017-10-08 20:17:57 -0700

    Abstract Modifier defined

--- CProject3/Polymorphism/AbstractClassesAndMembers.cs
+++ CProject3/Polymorphism/AbstractClassesAndMembers.cs
@@ -1,4 +1,7 @@
-﻿using System;
+﻿////Abstract Modifier = Indicates that a class or a member is missing implementation
+
+
+using System;
 namespace CProject3.Polymorphism
 {
     public class AbstractClassesAndMembers
