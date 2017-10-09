f69479b78f5baf4487ea7a39948c2858abf06eee
June Cho <junecho.ucb@gmail.com>
2017-10-08 20:25:23 -0700

    Defined Abstract classes

--- CProject3/Polymorphism/AbstractClassesAndMembers.cs
+++ CProject3/Polymorphism/AbstractClassesAndMembers.cs
@@ -1,5 +1,29 @@
 ﻿////Abstract Modifier = Indicates that a class or a member is missing implementation
 
+public class Shape
+{
+    public virtual void Draw()
+    {
+        ////Notice, that we can't state "Drawing a Shape"
+        /// In this case, we use "abstract" to indicate this Method is missing implementation
+        /// and we leave it to the dervied classes to provide an implementation for this Method
+    }
+
+    ////When declaring a class "abstract" note that you state it both in the class and method
+    ////Thus "abstract" are inherently "virtual", thus providing polymorphic behavior
+    public abstract class Shape
+    {
+        public abstract void Draw();
+    }
+}
+
+public class Circle : Shape
+{
+    public override void Draw()
+    {
+        System.Console.WriteLine("Drawing a Circle");
+    }
+}
 
 using System;
 namespace CProject3.Polymorphism
