f61786dd7178a7b2ad4573fefe947b6ae1656c05
June Cho <junecho.ucb@gmail.com>
2017-11-05 16:21:14 -0800

    Interface Defined

--- CProject3/Interfaces/Interfaces.cs
+++ CProject3/Interfaces/Interfaces.cs
@@ -1,4 +1,13 @@
-﻿using System;
+﻿#region Interfaces
+//What is an Interface?
+//A language that is similar to a class(in terms of syntax)
+//BUT is fundamentally different
+
+//
+
+
+#endregion
+using System;
 namespace CProject3.Interfaces
 {
     public class Interfaces
